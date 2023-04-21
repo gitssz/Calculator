06637dda92cd6dd885e97eab4170e6ec582baade
Geetanjali <83583240+gitssz@users.noreply.github.com>
2023-04-21 18:47:34 +0530

    add delete working

--- app.js
+++ app.js
@@ -5,25 +5,53 @@ const numberBtn = document.querySelectorAll(["data-number"]);
 const clearBtn = document.querySelector(".clear");
 const deleteBtn = document.querySelector(".delete");
 const equalsBtn = document.getElementById("equals");
-const btns=document.querySelectorAll(".btns");
-// var calculateValue;
-let num="";
+const dotBtn= document.querySelector(".dot");
+const btns = document.querySelectorAll(".btns");
+let num = "";
 window.onload = clear();
 
-btns.forEach(btn => {
-    btn.addEventListener("click",()=>{
-    let n2=currentOperand.innerHTML=num.concat(btn.innerHTML);
-    num=n2;  
-    })
-})
-
-equalsBtn.addEventListener("click",calculateValue);
-clearBtn.addEventListener("click",()=>{
-    currentOperand.innerHTML = "";
-    previousOperand.innerHTML = "";
-    num="";
+btns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    let n2 = (currentOperand.innerHTML = num.concat(btn.innerHTML));
+    num = n2;
+  });
 });
-deleteBtn.addEventListener("click",del(currentOperand.innerHTML));
+
+equalsBtn.addEventListener("click", calculateValue);
+clearBtn.addEventListener("click", () => {
+  currentOperand.innerHTML = "";
+  previousOperand.innerHTML = "";
+  num = "";
+});
+
+deleteBtn.addEventListener("click", () => {
+  currentOperand.innerHTML = currentOperand.innerHTML.slice(0, -1);
+  num = num.slice(0, -1);
+});
+
+function clear() {
+  currentOperand.innerHTML = "";
+  previousOperand.innerHTML = "";
+  // operation=undefined;
+}
+
+function appendNumber(number) {
+  if (number.innerHTML === "." && currentOperand.innerHTML.includes("."))
+    return;
+  currentOperand.innerHTML =
+    currentOperand.innerHTML.toString() + number.innerHTML.toString();
+}
+
+function chooseOperation(operator) {
+  this.operator = operator;
+  this.previousOperand = this.currentOperand;
+  this.currentOperand = "";
+}
+
+function calculateValue() {
+  previousOperand.innerHTML = currentOperand.innerHTML;
+  currentOperand.innerHTML = eval(currentOperand.innerHTML.toString());
+}
 
 // numberBtn.forEach((btn) => {
 //   btn.addEventListener("click", () => {
@@ -44,40 +72,6 @@ deleteBtn.addEventListener("click",del(currentOperand.innerHTML));
 //      this.previousOperand.innerHTML = this.previousOperand;
 //   }
 
-function clear() {
-  currentOperand.innerHTML = "";
-  previousOperand.innerHTML = "";
-  // operation=undefined;
-}
-
-function del(number){
-    number=number.slice(0,-1);
-    }
-    
-function appendNumber(number) {
-  if (number.innerHTML === "." && currentOperand.innerHTML.includes("."))return;
-  currentOperand.innerHTML=currentOperand.innerHTML.toString() + number.innerHTML.toString();
-}
-
-function chooseOperation(operator) {
-  this.operator = operator;
-  this.previousOperand = this.currentOperand;
-this.currentOperand="";
-}
-
-function calculateValue() {
-previousOperand.innerHTML=currentOperand.innerHTML;
-currentOperand.innerHTML=eval(currentOperand.innerHTML.toString()).toFixed(3);
-}
-
-
-
-
-
-
-
-
-
-
-
-
+// function del(number){
+//     number=number.slice(0,-1);
+//     }
